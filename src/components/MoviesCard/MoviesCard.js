a85200e24f23eb008d95ec7efa2b3cdd3806d507
Olga Ivanycheva <oly3998686@yandex.ru>
2023-09-29 08:16:30 +0300

    fix bag

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -8,60 +8,73 @@ import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import ModalTrailer from '../ModalTrailer/ModalTrailer';
 
 const MoviesCard = ({ movie, savedMovies, pinMovie, unpinMovie, mode }) => {
-    const [liked, setLiked] = useState(false);
-    const [showModal, setShowModal] = useState(false);
-    const location = useLocation();
-    const currentUser = useContext(CurrentUserContext);
+  const [liked, setLiked] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const location = useLocation();
+  const currentUser = useContext(CurrentUserContext);
 
-    useEffect(() => {
-        if (location.pathname === '/movies') {
-            const isOwner = savedMovies.some(
-                (savedMovie) => savedMovie.movieId === movie.id && savedMovie.owner === currentUser._id,
-            );
-            setLiked(isOwner);
-        }
-    }, []);
+  useEffect(() => {
+    if (location.pathname === '/movies') {
+      const isOwner = savedMovies.some(
+        (savedMovie) =>
+          savedMovie.movieId === movie.id &&
+          savedMovie.owner === currentUser._id
+      );
+      setLiked(isOwner);
+    }
+  }, []);
 
-    const handleButton = () => {
-        if (location.pathname === '/movies') {
-            liked ? unpinMovie(savedMovies.find((item) => item.movieId === movie.id)) : pinMovie(movie);
-        } else {
-            unpinMovie(movie);
-        }
-        setLiked((state) => !state);
-    };
+  const handleButton = () => {
+    if (location.pathname === '/movies') {
+      liked
+        ? unpinMovie(savedMovies.find((item) => item.movieId === movie.id))
+        : pinMovie(movie);
+    } else {
+      unpinMovie(movie);
+    }
+    setLiked((state) => !state);
+  };
 
-    const handleOpenModal = () => {
-        setShowModal(true);
-    };
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
 
-    const handleCloseModal = () => {
-        setShowModal(false);
-    };
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
 
-    const name = movie.nameRU;
-    const duration = convertToHoursMinutes(movie.duration);
-    const urlImage = location.pathname === '/movies' ? `${BASE_IMAGE_URL}${movie.image.url}` : movie.image;
-    const trailerLink = movie.trailerLink;
+  const name = movie.nameRU;
+  const duration = convertToHoursMinutes(movie.duration);
+  const urlImage =
+    location.pathname === '/movies'
+      ? `${BASE_IMAGE_URL}${movie.image.url}`
+      : movie.image;
+  const trailerLink = movie.trailerLink;
 
-    return (
-        <li className="movies__card">
-            <div className="movies__card-info">
-                <p className="movies__card-title">{name}</p>
-                <p className="movies__card-duration">{duration}</p>
-                <button
-                    className={`movies__card-button ${liked ? 'movies__card-button_liked' : ''} ${
-                        mode === 'liked' ? 'movies__card-button_pined' : ''
-                    }`}
-                    onClick={handleButton}
-                />
-            </div>
-            <button className="link movies__card-trailerLink" onClick={handleOpenModal}>
-                <img className="movies__card-image" src={urlImage} alt="Фото" />
-            </button>
-            {showModal && <ModalTrailer onClose={handleCloseModal} trailerLink={trailerLink} />}
-        </li>
-    );
+  return (
+    <li className="movies__card">
+      <button
+        className="link movies__card-trailerLink"
+        onClick={handleOpenModal}
+      >
+        <img className="movies__card-image" src={urlImage} alt="Фото" />
+      </button>
+      <div className="movies__card-info">
+        <p className="movies__card-title">{name}</p>
+        <button
+          className={`movies__card-button ${
+            liked ? 'movies__card-button_liked' : ''
+          } ${mode === 'liked' ? 'movies__card-button_pined' : ''}`}
+          onClick={handleButton}
+        />
+        <div className="movies__card-row"></div>
+        <p className="movies__card-duration">{duration}</p>
+      </div>
+      {showModal && (
+        <ModalTrailer onClose={handleCloseModal} trailerLink={trailerLink} />
+      )}
+    </li>
+  );
 };
 
 export default MoviesCard;
